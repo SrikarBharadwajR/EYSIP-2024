214f3428f85191880ad5c8dfcae06cb40cbb78ad
Srikar Bharadwaj R <65727189+SrikarBharadwajR@users.noreply.github.com>
2024-07-21 13:48:02 +0530

    BLE_with_IMU_Main

---
+++ BLE_with_IMU_test/pca10040/s132/ses/Output/Debug/Obj/BLE_with_IMU/app_error.d
@@ -0,0 +1,27 @@
+Output/Debug/Obj/BLE_with_IMU/app_error.o: \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.c \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL_ConfDefaults.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL_Conf.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL_Arm_Conf.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/stdio.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/stdbool.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nordic_common.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error_weak.h
---
+++ BLE_with_IMU_test/pca10040/s132/ses/Output/Debug/Obj/BLE_with_IMU/app_util_platform.d
@@ -0,0 +1,35 @@
+Output/Debug/Obj/BLE_with_IMU/app_util_platform.o: \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_util_platform.c \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_util_platform.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL_ConfDefaults.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL_Conf.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/__SEGGER_RTL_Arm_Conf.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_nvic.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nrf_assert.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/stdio.h \
+ /usr/share/segger_embedded_studio_for_arm_7.32a/include/stdbool.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/nordic_common.h \
+ /mnt/Storage/Projects/Segger/nRF5_SDK_17.1.0_ddde560/components/libraries/util/app_error_weak.h
